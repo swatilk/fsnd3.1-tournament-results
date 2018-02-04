f62e908dd0daedc1e3bd6c3ab6ab0e40a2d887e5
Swati Kamtar <swatilk@users.noreply.github.com>
2018-02-03 16:38:21 -0800

    Change players table datatype from str to varchar

--- tournament.sql
+++ tournament.sql
@@ -6,7 +6,7 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-create table players (player_id SERIAL primary key, full_name str(20));
+create table players (player_id SERIAL primary key, full_name varchar(20));
 create table matches (match_id SERIAL primary key, winner int, loser int, foreign key (winner) references players (player_id), foreign key (loser) references players (player_id));
 create view standings as select player_id, full_name, (select count(matches.winner) from matches where player_id = matches.winner) as wins,(select count(matches.match_id) from matches where player_id = matches.winner OR player_id = matches.loser) as matches from Players Order by wins desc, matches desc;
 
